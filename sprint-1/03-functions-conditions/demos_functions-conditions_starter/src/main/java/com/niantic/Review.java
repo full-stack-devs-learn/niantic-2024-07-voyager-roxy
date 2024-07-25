44d294985814554743d5e33c6533941b768cf4eb
Roxy Abedi <roxy.abedi@gmail.com>
2024-07-24 17:52:33 -0700

    float vs double demo

---
+++ sprint-1/03-functions-conditions/demos_functions-conditions_starter/src/main/java/com/niantic/Review.java
@@ -0,0 +1,11 @@
+package com.niantic;
+
+public class Review {
+    public static void main(String[] args) {
+        float smallerNumber = Float.MAX_VALUE;
+        double biggerNumber = Double.MAX_VALUE;
+
+        System.out.println(smallerNumber);
+        System.out.println(biggerNumber);
+    }
+}
