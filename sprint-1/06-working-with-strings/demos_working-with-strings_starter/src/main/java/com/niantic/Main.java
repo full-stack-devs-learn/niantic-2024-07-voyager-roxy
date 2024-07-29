4bfb05bac819bba406390a2a38199f8c6a783453
Roxy Abedi <roxy.abedi@gmail.com>
2024-07-29 13:19:33 -0700

    demo code for phone number reformatting

--- sprint-1/06-working-with-strings/demos_working-with-strings_starter/src/main/java/com/niantic/Main.java
+++ sprint-1/06-working-with-strings/demos_working-with-strings_starter/src/main/java/com/niantic/Main.java
@@ -1,9 +1,34 @@
 package com.niantic;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+public class Main {
+    public static void main(String[] args) {
+        String formattedPhone = formatPhone2("(800)555-1234");
+        System.out.println(formattedPhone);
     }
+
+    //option 1 format
+    public static String formatPhone(String originalPhone) {
+        char[] chars = new char[10];
+        int count = 0;
+
+        for (int i = 0; i < originalPhone.length(); i++) {
+            char currentChar = originalPhone.charAt(i);
+            if (Character.isDigit(currentChar)) {
+                chars[count] = currentChar;
+                count++;
+            }
+        }
+
+        return new String(chars);
+    }
+
+    //option 2 format
+    public static String formatPhone2(String originalPhone) {
+        String areaCode = originalPhone.substring(1, 4);
+        String prefix = originalPhone.substring(5, 8);
+        String lastFour = originalPhone.substring(9);
+
+        return areaCode + prefix + lastFour;
+    }
+
 }
