139498f9a45775e7331702ff97a3d37217559d0c
Roxy Abedi <roxy.abedi@gmail.com>
2024-08-06 00:04:02 -0700

    completed challenge exercises

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Hotel.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Hotel.java
@@ -2,4 +2,74 @@ package com.niantic.part_3_challenge;
 
 public class Hotel
 {
+
+    private int numberOfRooms;
+    private int numberOfSuites;
+    private int bookedRooms;
+    private int bookedSuites;
+    private int availableRooms;
+    private int availableSuites;
+
+    public Hotel(int numberOfRooms, int numberOfSuites)
+    {
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfSuites = numberOfSuites;
+    }
+
+    public Hotel(int numberOfRooms, int numberOfSuites, int bookedRooms, int bookedSuites)
+    {
+        this.numberOfRooms = numberOfRooms;
+        this.numberOfSuites = numberOfSuites;
+        this.bookedRooms = bookedRooms;
+        this.bookedSuites = bookedSuites;
+        this.availableRooms = numberOfRooms - bookedRooms;
+        this.availableSuites = numberOfSuites - bookedSuites;
+    }
+
+    public int getNumberOfRooms()
+    {
+        return numberOfRooms;
+    }
+
+    public int getNumberOfSuites()
+    {
+        return numberOfSuites;
+    }
+
+    public int getBookedRooms()
+    {
+        return bookedRooms;
+    }
+
+    public int getBookedSuites()
+    {
+        return bookedSuites;
+    }
+
+    public int getAvailableRooms()
+    {
+        return availableRooms;
+    }
+
+    public int getAvailableSuites()
+    {
+        return availableSuites;
+    }
+
+    public boolean makeReservation(int numberOfRooms, boolean isSuite)
+    {
+        if(isSuite && (numberOfSuites - bookedSuites > 0) && numberOfRooms < availableSuites)
+        {
+            bookedSuites += numberOfRooms;
+            availableSuites -= numberOfRooms;
+            return true;
+        }
+        if(!isSuite && numberOfRooms < availableRooms)
+        {
+            bookedRooms += numberOfRooms;
+            availableRooms -= numberOfRooms;
+            return true;
+        }
+        return false;
+    }
 }
--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_3_challenge/Refrigerator.java
@@ -2,4 +2,68 @@ package com.niantic.part_3_challenge;
 
 public class Refrigerator
 {
+    private final int MAX_TEMPERATURE = 35;
+    private final int MIN_TEMPERATURE = 0;
+
+
+    private int maxCapacity;
+    private int currentTemperature;
+    private int available;
+    private boolean isDoorOpen = false;
+
+    public Refrigerator(int currentTemperature, int maxCapacity)
+    {
+        this.currentTemperature = currentTemperature;
+        this.maxCapacity = maxCapacity;
+        this.available = maxCapacity;
+    }
+
+    public int getCurrentTemperature(){
+        return currentTemperature;
+    }
+
+    public int getMaxCapacity()
+    {
+        return maxCapacity;
+    }
+
+    public int getAvailableCapacity()
+    {
+        return isDoorOpen ? available : 0;
+    }
+
+    public boolean isDoorOpen()
+    {
+        return isDoorOpen;
+    }
+
+    public void openDoor()
+    {
+        isDoorOpen = true;
+    }
+
+    public void closeDoor()
+    {
+        isDoorOpen = false;
+    }
+
+    public boolean addItem(int capacity)
+    {
+        if(isDoorOpen && capacity <= maxCapacity)
+        {
+            available -= capacity;
+            return true;
+        }
+        else return false;
+    }
+
+    public boolean removeItem(int capacity)
+    {
+        if(isDoorOpen && (available - capacity) >= 0)
+        {
+            available += capacity;
+            return true;
+        }
+        else return false;
+    }
 }
