8250f6148bca42f4986d740cc41f73b3833ac6be
Roxy Abedi <roxy.abedi@gmail.com>
2024-08-20 13:07:23 -0700

    completed wizard exercise

--- sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
+++ sprint-2/02-inheritance/exercise_inheritance/src/main/java/com/niantic/Wizard.java
@@ -1,5 +1,46 @@
 package com.niantic;
 
-public class Wizard
+public class Wizard extends Character
 {
+
+    private int mana;
+
+    public Wizard(String name, int health, int level, int experience, int mana) {
+        super(name, health, level, experience);
+
+        this.mana = mana;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public void castSpell(Character target)
+    {
+        if(mana >= 10 && !isDefeated()) {
+            super.attack(target);
+            super.attack(target);
+            mana -= 10;
+        }
+    }
+
+    public void regenerateMana(int amount)
+    {
+        if(!isDefeated()){
+            mana += amount;
+        }
+    }
+
+    @Override
+    public void levelUp()
+    {
+        super.levelUp();
+        if(!isDefeated()) mana += 10;
+    }
+
+    @Override
+    public String specialAbility()
+    {
+        return "Cast Spells";
+    }
 }
