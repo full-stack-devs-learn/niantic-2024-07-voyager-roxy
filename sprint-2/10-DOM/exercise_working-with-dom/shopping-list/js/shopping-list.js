689c1bdf3f3d8c3687b124308434028c2041942e
Roxy Abedi <roxy.abedi@gmail.com>
2024-08-30 17:54:05 -0700

    completed assignment

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -2,22 +2,87 @@
 
 // add groceries
 
+const service = new ShoppingService();
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() {
+    const listTitle = document.getElementById("title");
+    listTitle.textContent = service.getListName();
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+    function displayGroceries() {
+    const groceries = service.getShoppingList();
+
+    const groceryContainer = document.getElementById("shopping-list");
+
+    groceries.forEach(grocery => {
+        createGroceryDiv(grocery, groceryContainer);
+    });
+}
+
+function createGroceryDiv(grocery, parent)
+{
+    const groceryDiv = document.createElement("div");
+    groceryDiv.classList.add("list-item");
+
+    if(grocery.isComplete){
+        groceryDiv.classList.add("complete");
+    }
+
+    groceryDiv.textContent = grocery.title;
+
+    parent.appendChild(groceryDiv);
+
+    buildQuantityDiv(grocery, groceryDiv)
+}
+
+function buildQuantityDiv(grocery, parent)
+{
+    const quantityDiv = document.createElement("div");
+
+    quantityDiv.classList.add("quantity-container");
+
+    buildSpanElement(quantityDiv);
+
+    const textContent = document.createTextNode(grocery.quantity);
+
+    quantityDiv.appendChild(textContent);
+    
+    parent.appendChild(quantityDiv);
+}
+
+function buildSpanElement(parent)
+{
+    const spanElement = document.createElement("span");
+
+    spanElement.classList.add("super");
+
+    spanElement.textContent = "quantity";
+
+    parent.appendChild(spanElement);
+}
+
+
+
+
+
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
 function markCompleted() {
+    const groceries = document.querySelectorAll(".list-item");
+
+
+    groceries.forEach(grocery => {
+        grocery.classList.add("complete");
+    });
 }
 
 
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,6 +1,7 @@
 class ShoppingService {
 
     getListName() {
+        return 'Shopping List';
     }
 
     getShoppingList() {
@@ -16,6 +17,62 @@ class ShoppingService {
                 title: 'Frozen Pizza',
                 quantity: 4,
                 isComplete: true
+            }
+            ,
+            {
+                id: 3,
+                title: 'Apples',
+                quantity: 6,
+                isComplete: true
+            }
+            ,
+            {
+                id: 4,
+                title: 'Cereal',
+                quantity: 1,
+                isComplete: false
+            }
+            ,
+            {
+                id: 5,
+                title: 'Milk',
+                quantity: 1,
+                isComplete: true
+            }
+            ,
+            {
+                id: 6,
+                title: 'Yogurt',
+                quantity: 5,
+                isComplete: false
+            }
+            ,
+            {
+                id: 7,
+                title: 'Bagels',
+                quantity: 8,
+                isComplete: false
+            }
+            ,
+            {
+                id: 8,
+                title: 'Pasta Sauce',
+                quantity: 2,
+                isComplete: true
+            }
+            ,
+            {
+                id: 9,
+                title: 'Onions',
+                quantity: 3,
+                isComplete: true
+            }
+            ,
+            {
+                id: 10,
+                title: 'Ice Cream',
+                quantity: 4,
+                isComplete: false
             }
         ];
     }
