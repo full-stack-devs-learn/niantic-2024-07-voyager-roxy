388609e2fb270294b42ab3765a06ad96eabd8fb6
Roxy Abedi <roxy.abedi@gmail.com>
2024-09-16 14:33:36 -0700

    completed card and hand exercise

--- sprint-3/03-polymorphism-review/exercise_polymorphism-interfaces/src/main/java/com/niantic/models/Card.java
+++ sprint-3/03-polymorphism-review/exercise_polymorphism-interfaces/src/main/java/com/niantic/models/Card.java
@@ -1,38 +1,33 @@
 package com.niantic.models;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Card implements Comparable<Card>
-{
+public class Card implements Comparable<Card> {
     private String suit;
     private String faceValue;
 
-    public Card(String suit, String faceValue)
-    {
+    public Card(String suit, String faceValue) {
         this.suit = suit;
         this.faceValue = faceValue;
     }
 
-    public String getSuit()
-    {
+    public String getSuit() {
         return suit;
     }
 
-    public String getFaceValue()
-    {
+    public String getFaceValue() {
         return faceValue;
     }
 
-    public int getPointValue()
-    {
+    public int getPointValue() {
         return cardValues.get(faceValue);
     }
 
-    public String getColor()
-    {
-        switch (suit.toLowerCase())
-        {
+    public String getColor() {
+        switch (suit.toLowerCase()) {
             case "hearts":
             case "diamonds":
                 return "Red";
@@ -42,8 +37,7 @@ public class Card implements Comparable<Card>
     }
 
     // lookup map
-    private static final Map<String, Integer> cardValues = new HashMap<>()
-    {{
+    private static final Map<String, Integer> cardValues = new HashMap<>() {{
         put("A", 11);
         put("K", 10);
         put("Q", 10);
@@ -59,10 +53,34 @@ public class Card implements Comparable<Card>
         put("2", 2);
     }};
 
+    private static final Map<String, Integer> suitValues = new HashMap<>() {{
+        put("Clubs", 1);
+        put("Diamonds", 2);
+        put("Hearts", 3);
+        put("Spades", 4);
+    }};
+
+    private static final Map<String, Integer> faceValueOrder = new HashMap<>() {{
+        put("10", 1);
+        put("J", 2);
+        put("Q", 3);
+        put("K", 4);
+
+    }};
+
     @Override
-    public int compareTo(Card o)
-    {
+    public int compareTo(Card o) {
         // todo: Exercise 1: implement Comparable<Card>
-        return 0;
+        if (!o.getSuit().equals(this.getSuit())) {
+            return suitValues.get(o.getSuit()) < suitValues.get(this.getSuit()) ? -1 : 1;
+        }
+        else {
+            if (this.getPointValue() != o.getPointValue()) {
+                return this.getPointValue() < o.getPointValue() ? -1 : 1;
+            }
+            else {
+                return faceValueOrder.get(this.getFaceValue()) < faceValueOrder.get(o.getFaceValue()) ? -1 : 1;
+            }
+        }
     }
 }
--- sprint-3/03-polymorphism-review/exercise_polymorphism-interfaces/src/main/java/com/niantic/models/Hand.java
+++ sprint-3/03-polymorphism-review/exercise_polymorphism-interfaces/src/main/java/com/niantic/models/Hand.java
@@ -1,6 +1,7 @@
 package com.niantic.models;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Hand
 {
@@ -24,7 +25,7 @@ public class Hand
     public void Sort()
     {
         // Todo: Exercise 2: implement this sort method
-
+        Collections.sort(cards);
     }
 
     public int getCardCount()
@@ -36,5 +37,6 @@ public class Hand
     public void dealTo(Card card)
     {
         cards.add(card);
+        Sort();
     }
 }
