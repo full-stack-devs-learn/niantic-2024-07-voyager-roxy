35b79098b31bf87ad56c7dcb2d285914468b5847
Roxy Abedi <roxy.abedi@gmail.com>
2024-09-20 11:13:54 -0700

    updated the ProductController and created HttpError Class

--- sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/ProductsController.java
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/ProductsController.java
@@ -1,9 +1,13 @@
 package com.niantic.controllers.apis;
 
+import com.niantic.models.HttpError;
 import com.niantic.models.Product;
+import com.niantic.services.CategoryDao;
+import com.niantic.services.MySqlCategoryDao;
 import com.niantic.services.MySqlProductDao;
 import com.niantic.services.ProductDao;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,11 +15,24 @@ import java.util.List;
 @RestController
 public class ProductsController {
     private ProductDao productDao = new MySqlProductDao();
+    private CategoryDao categoryDao = new MySqlCategoryDao();
 
     @GetMapping("/api/products")
-    public List<Product> getProductsByCategory(@RequestParam int catId)
+    public ResponseEntity<?> getProductsByCategory(@RequestParam int catId)
     {
-        return productDao.getProductsByCategory(catId);
+        try {
+            var products = productDao.getProductsByCategory(catId);
+            var category = categoryDao.getCategory(catId);
+            if(category == null)
+            {
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(products);
+        }
+        catch (Exception e)
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @GetMapping("/api/products/{productId}")
