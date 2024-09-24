8a5574b4d8a339943fc07eec889dc2f6b30ed332
Roxy Abedi <roxy.abedi@gmail.com>
2024-09-24 14:57:31 -0700

    added click event to display employee details

--- sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-row-container/EmployeesRowContainer.jsx
+++ sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-row-container/EmployeesRowContainer.jsx
@@ -1,19 +1,41 @@
 import EmployeesRow from "../employees-row/EmployeesRow"
 import { employees } from '../../../data'
+import { useState } from "react"
 
 
-export default function EmployeesRowContainer()
-{
-    return(
+export default function EmployeesRowContainer() {
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
+
+    const employeeSelected = (id) => {
+        const employee = employees.find(person => person.employeeId === id)
+        setSelectedEmployee(employee)
+        console.log(employee)
+    }
+
+    return (
         <main className="container mt-4 employees-container" id="employees-container">
-            {
-                employees.map((employee) => (
-                    <EmployeesRow key={employee.employeeId}
-                    id={employee.employeeId}
-                    name={`${employee.firstName} ${employee.lastName}`}
-                    title={employee.title}></EmployeesRow>
-                ))
-            }
+            {selectedEmployee && (
+                <div className="employee-row">
+                    <img id="employee-image" src={`images/employees/${selectedEmployee.employeeId}.webp`} style={{ width: "200px", height: "auto" }} />
+                    <div className="employee-name">
+                        <h1>{`${selectedEmployee.firstName} ${selectedEmployee.lastName}`}</h1>
+                        <h6>Title: {selectedEmployee.title}</h6>
+                        <h6>Salary: ${selectedEmployee.salary}</h6>
+                        <h6>Notes: {selectedEmployee.notes}</h6>
+                    </div>
+                </div>
+            )}
+            <div>
+                {
+                    employees.map((employee) => (
+                        <EmployeesRow key={employee.employeeId}
+                            id={employee.employeeId}
+                            name={`${employee.firstName} ${employee.lastName}`}
+                            title={employee.title}
+                            onEmployeeSelected={employeeSelected}></EmployeesRow>
+                    ))
+                }
+            </div>
         </main>
-    )
+    );
 }
--- sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-row/EmployeesRow.jsx
+++ sprint-3/10-introduction-to-react/exercise_northwind-react/src/components/employees/employees-row/EmployeesRow.jsx
@@ -1,10 +1,14 @@
 
-export default function EmployeesRow({id, name, title})
+export default function EmployeesRow({id, name, title, onEmployeeSelected})
 {
     const imageUrl = `images/employees/${id}.webp`
+
+    const employeeClicked = () => {
+        onEmployeeSelected(id)
+    }
     
     return(
-        <div className="employee-row">
+        <div className="employee-row" onClick={employeeClicked} style={{cursor: 'pointer'}}>
           <img id="employee-image" src={imageUrl} />
           <div className="employee-name">
             <h1>{name}</h1>
