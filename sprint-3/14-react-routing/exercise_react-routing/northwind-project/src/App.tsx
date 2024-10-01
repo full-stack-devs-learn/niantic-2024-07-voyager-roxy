2c99a9b1b9b8c8dffeae117dac106c267df202dc
Roxy Abedi <roxy.abedi@gmail.com>
2024-09-30 19:58:37 -0700

    create and set up new project structure

---
+++ sprint-3/14-react-routing/exercise_react-routing/northwind-project/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/header/Header'
+import '../node_modules/bootswatch/dist/morph/bootstrap.css'
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default App
---
+++ sprint-3/14-react-routing/exercise_react-routing/northwind-project/src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+export default function Header() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Northwind</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarColor01">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <a className="nav-link active" href="#">Home
+                                <span className="visually-hidden">(current)</span>
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Categories</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Products</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
